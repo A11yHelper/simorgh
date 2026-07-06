6c366055a6874b44067ac613ac709a52fbb52616
ldt1997 <474376875@qq.com>
2026-07-06 17:14:31 +0200

    submit T3

--- src/app/components/MostRead/TopStories/index.tsx
+++ src/app/components/MostRead/TopStories/index.tsx
@@ -1,8 +1,55 @@
+import styled from '@emotion/styled';
 import { MostReadData } from '../types';
 
 interface TopStoriesProps {
   data: MostReadData;
 }
+
+const Section = styled.section({
+  marginTop: '1.5rem',
+});
+
+const Heading = styled.h2({
+  fontSize: '1.5rem',
+  lineHeight: 1.2,
+  margin: '0 0 1rem',
+});
+
+const List = styled.ul({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  margin: 0,
+  padding: 0,
+  listStyle: 'none',
+});
+
+const ListItem = styled.li({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.375rem',
+});
+
+const TitleLink = styled.a({
+  color: 'inherit',
+  textDecoration: 'none',
+
+  '&:hover, &:focus-visible': {
+    textDecoration: 'underline',
+  },
+});
+
+const Meta = styled.time({
+  color: '#5a5a5a',
+  fontSize: '0.9375rem',
+});
+
+const formatReadableDate = (value: Date) =>
+  new Intl.DateTimeFormat('en-GB', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(value);
 
 /**
  * TODO: TASK T3
@@ -26,7 +73,25 @@ interface TopStoriesProps {
  */
 
 const TopStories = ({ data }: TopStoriesProps) => {
-  return <h1>TopStories</h1>;
+  return (
+    <Section id="topStories" aria-labelledby="topStoriesHeading">
+      <Heading id="topStoriesHeading">Top Stories</Heading>
+      <List>
+        {data.items.slice(0, 3).map(item => {
+          const publishedDate = new Date(item.timestamp);
+
+          return (
+            <ListItem key={item.id}>
+              <TitleLink href={item.href}>{item.title}</TitleLink>
+              <Meta dateTime={publishedDate.toISOString()}>
+                {formatReadableDate(publishedDate)}
+              </Meta>
+            </ListItem>
+          );
+        })}
+      </List>
+    </Section>
+  );
 };
 
 export default TopStories;
